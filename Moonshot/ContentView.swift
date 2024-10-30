724e3bad16b9f7e9be4f43de6f2f1cb10c1e2b23
Razvan Pricop <razvanipricop@gmail.com>
2024-10-30 01:10:35 +0100

    Improve navigation data and remove layout choice

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,28 +10,46 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    @State private var viewPreference = false
+    let columns = [GridItem(.adaptive(minimum: 150))]
     
     var body: some View {
         NavigationStack {
-            Group {
-                if viewPreference {
-                    GridView(astronauts: astronauts, missions: missions)
-                } else {
-                    ListView(astronauts: astronauts, missions: missions)
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink(value: mission) {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.lightBackground))
+                        }
+                    }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .navigationDestination(for: Mission.self) { mission in
+                MissionView(mission: mission, astronauts: astronauts)
+            }
             .background(.darkBackground)
             .preferredColorScheme(.dark)
-            .toolbar {
-                Button("List") {
-                    viewPreference = true
-                }
-                Button("Grid") {
-                    viewPreference = false
-                }
-            }
         }
     }
 }
